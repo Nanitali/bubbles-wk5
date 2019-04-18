7baa28b1223ab3bc8ca08e717142d9d2977ff3bf
Yunan Li <amy_tako@hotmail.com>
2019-04-19 11:00:37 +1200

    change res.send to res.json

--- server/routes/ducks.js
+++ server/routes/ducks.js
@@ -6,7 +6,7 @@ const db = require('../db/db')
 router.get('/', (rqe, res) => {
   db.getDucks()
     .then(ducks => {
-      res.send(ducks)
+      res.json(ducks)
     })
     .catch(err => {
       res.status(500).send(err.message)
