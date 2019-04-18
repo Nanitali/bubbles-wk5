578387c55801ec012bc140ba428242475330459a
Yunan Li <amy_tako@hotmail.com>
2019-04-19 11:57:04 +1200

    add form done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import DuckForm from './DuckForm'
+
 import { getDucks } from '../api'
 
 const App = () => {
   getDucks()
     .then(ducks => console.log(ducks))
   return (
-    <h1>React development has begun!</h1>
+    <DuckForm />
   )
 }
 
---
+++ client/components/DuckForm.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+export default class DuckFrom extends React.Component {
+  state = {
+    message: '',
+    name: '',
+    lat: '',
+    lng: '',
+    contact: '',
+    details: '',
+    timeframe: '',
+    type: ''
+  }
+
+  changeHandler = e => {
+    let message = ''
+    if (e.target.name === 'lat' || e.target.name === 'lng') {
+      if (isNaN(e.target.value)) {
+        message = 'Lat and lng should be numbers'
+      }
+    }
+    this.setState({
+      [e.target.name]: e.target.value,
+      message
+    })
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+  }
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          Name: <input
+            type='text'
+            value={this.state.name}
+            onChange={this.changeHandler}
+            placeholder='Your name'
+            name='name'/>
+          <br/>
+          <br/>
+          Location: <input
+            type='text'
+            value={this.state.lat}
+            onChange={this.changeHandler}
+            placeholder='lat'
+            name='lat'/>
+          <input
+            type='text'
+            value={this.state.lng}
+            onChange={this.changeHandler}
+            placeholder='lng'
+            name='lng'/>
+          <br/>
+          <br/>
+          Details: <input
+            type='text'
+            value={this.state.details}
+            onChange={this.changeHandler}
+            placeholder='Need help with / are offering?'
+            name='details'/>
+          <br/>
+          <br/>
+          Timeframe: <input
+            type='text'
+            value={this.state.timeframe}
+            onChange={this.changeHandler}
+            placeholder='When?'
+            name='timeframe'/>
+          <br/>
+          <br/>
+          Type: <input
+            type='text'
+            value={this.state.type}
+            onChange={this.changeHandler}
+            placeholder='Request/Offer'
+            name='type'/>
+          <br/>
+          <br/>
+          <input
+            type='submit'
+            value='Add'/>
+        </form>
+        <p>{this.state.message}</p>
+      </div>
+    )
+  }
+}
