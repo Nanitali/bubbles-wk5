872075844f0cbbf5264c3753351be018e56e8c8c
Yunan Li <amy_tako@hotmail.com>
2019-04-19 10:24:41 +1200

    add table ducks

---
+++ migrations/20190419095531_ducks.js
@@ -0,0 +1,17 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('ducks', table => {
+    table.increments('id').primary()
+    table.integer('lat')
+    table.integer('lng')
+    table.string('name')
+    table.string('contact')
+    table.string('details')
+    table.string('timeframe')
+    table.string('type')
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('ducks')
+
+}
